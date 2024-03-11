a7752f782475278d4154468364547ee70269601c
MandichRiccardo <ricky.mandich@gmail.com>
2024-03-11 16:04:21 +0100

    aggiornamento 2024 03 11 16:05

--- Main.py
+++ Main.py
@@ -1,24 +1,24 @@
-#esercizio 1
+# esercizio 1
 numero_intero = 3546523
 print(type(numero_intero))
-#esercizio 2
+# esercizio 2
 numero_decimale = 1.618
 print(type(numero_decimale))
-#esercizio 3
+# esercizio 3
 testo = "miaFrase"
 print(type(testo))
-#esercizio 4
+# esercizio 4
 valore_booleano = False
 print(type(valore_booleano))
-#esercizio 5
+# esercizio 5
 numeri = list(range(1, 6))
 print(type(numeri))
-#esercizio 6
+# esercizio 6
 misti = [numero_intero, numero_decimale, testo, valore_booleano]
 print(type(misti))
-#esercizio 7
+# esercizio 7
 giorni_settimana = ("Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica")
 print(type(giorni_settimana))
-#esercizio 8
-informazioni_personali = {"nome" : "Riccardo", "cognome" : "Mandich", "eta" : 17, "Citta" : "Marghera"}
+# esercizio 8
+informazioni_personali = {"nome": "Riccardo", "cognome": "Mandich", "eta": 17, "Città": "Marghera"}
 print(type(informazioni_personali))
